9b7c3a01d4f67a612ad4a8b1d66a8101845bab71
stefanboeck <ckRENhETQoJHVJEANG7BBwuw>
2025-09-17 23:30:34 +0200

    Update

---
+++ OpenPIMS/OpenPIMS MobilTests/OpenPIMS_MobilTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenPIMS_MobilTests.swift
+//  OpenPIMS MobilTests
+//
+//  Created by Stefan Böck on 17.09.25.
+//
+
+import Testing
+@testable import OpenPIMS_Mobil
+
+struct OpenPIMS_MobilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
